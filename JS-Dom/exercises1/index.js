79afce7388ae5a94787440461870db7a3a0321ff
Khegal <Khegal95@gmail.com>
2024-07-30 17:52:34 +0800

    main

--- JS-Dom/exercises1/index.js
+++ JS-Dom/exercises1/index.js
@@ -1,15 +1,21 @@
-console.log("project is up");
-
 // 👉 TASK 1- Select the following elements from the DOM:
 
 // BUTTONS
 const launchButton = null;
+console.log(document.getElementById("launchButton").innerHTML);
+
 const confirmButton = null;
+console.log(document.getElementById("confirmButton").innerHTML);
+
 const cancelButton = null;
+console.log(document.getElementById("cancelButton").innerHTML);
 
 // MESSAGES
 const successMessage = null;
+console.log(document.getElementsByClassName("report success off"));
+
 const failureMessage = null;
+console.log(document.getElementsByClassName("report failure off"));
 
 // MODAL
 const modal = null;
